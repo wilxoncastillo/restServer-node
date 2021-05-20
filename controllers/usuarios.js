d0cb89685c639b7c27d8feda20feec82391db08f
wilxoncastillo <wilxoncastillo@gmail.com>
2021-05-20 15:18:53 -0400

    130 Retornar el total de registros en una coleccion

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -12,12 +12,27 @@ const usuariosGet = async(req = request, res = response) => {
 	// destruracon de argumentos
 
 	const { limite = 5, desde = 0} = req.query;
+	const query = {estado: true};
 
-	const usuarios = await Usuario.find()
+	/*
+	const total = await Usuario.countDocuments(query);
+
+	const usuarios = await Usuario.find(query)
 		.skip(Number(desde))
 		.limit(Number(limite));
 
+	*/
+
+	const [ total, usuarios ] = await Promise.all([
+		Usuario.countDocuments(query),
+
+		Usuario.find(query)
+			.skip(Number(desde))
+			.limit(Number(limite))
+	]);
+
 	res.json({
+		total,
 		usuarios
 	});
 };
