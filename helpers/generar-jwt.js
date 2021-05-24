9e460ecaf7af9458577f17f943006b0ae2306c97
Wilson Castillo <wilxoncastillo@gmail.com>
2021-05-24 15:41:53 -0400

    148 Optimizar importaciones node

--- helpers/generar-jwt.js
+++ helpers/generar-jwt.js
@@ -5,7 +5,7 @@ const generarJWT = (uid = '') => {
 		const payload = { uid };
 
 		jwt.sign(payload, process.env.SECRETTOPRIVATEKEY, {
-			expiresIn: '4h'
+			expiresIn: '1000h'
 		}, (err, token) =>{
 			if(err) {
 				console.log(err);
